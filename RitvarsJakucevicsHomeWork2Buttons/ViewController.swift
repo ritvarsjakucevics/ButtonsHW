7450888888e01b429aebb594b0b68bd9f2786ede
Ritvars Jakucevičs <ritvarsjakucevics@gmail.com>
2019-10-22 12:02:50 +0300

    Initial Commit

---
+++ RitvarsJakucevicsHomeWork2Buttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RitvarsJakucevicsHomeWork2Buttons
+//
+//  Created by Ritvars Jakucevičs on 22/10/2019.
+//  Copyright © 2019 Ritvars Jakucevičs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
